c81233961fa3bad4db0056d1a4838ae5731c572d
Dobromir Nikolov <dobromir.nikolov@immedis.com>
2018-09-05 15:29:24 +0300

    A bit of code reorganization.

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,7 @@ import CounterApi
 import CounterEventStore
 import Eventful.Store.Memory
 import Network.Wai.Handler.Warp
-import Network.Wai.Logger (withStdoutLogger)
+import Network.Wai.Logger (ApacheLogger, withStdoutLogger)
 import Servant
 
 port :: Int
@@ -20,10 +20,14 @@ getEventStoreReaderWriter = do
 app :: CounterEventReader -> CounterEventWriter -> Application
 app reader writer = serve counterApiProxy $ server reader writer
 
+configureLoggerSettings :: ApacheLogger -> Settings
+configureLoggerSettings logger = setPort port $ setLogger logger defaultSettings
+
 runApp :: CounterEventReader -> CounterEventWriter -> IO ()
-runApp reader writer = withStdoutLogger $ \logger -> do
-                                                     let settings = setPort port $ setLogger logger defaultSettings
-                                                     runSettings settings $ app reader writer
+runApp reader writer = withStdoutLogger $ runWithSettings . configureLoggerSettings
+    where 
+        runWithSettings :: Settings -> IO ()
+        runWithSettings settings = runSettings settings $ app reader writer
 
 main :: IO ()
 main = do
