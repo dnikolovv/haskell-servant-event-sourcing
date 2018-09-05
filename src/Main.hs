1d68b3165ef22ed928e2970e7a590aa7f1eefd5c
Dobromir Nikolov <dobromir.nikolov@immedis.com>
2018-09-05 15:17:48 +0300

    Added Servant logging.

--- src/Main.hs
+++ src/Main.hs
@@ -4,18 +4,29 @@ import CounterApi
 import CounterEventStore
 import Eventful.Store.Memory
 import Network.Wai.Handler.Warp
+import Network.Wai.Logger (withStdoutLogger)
 import Servant
 
 port :: Int
 port = 8081
 
+getEventStoreReaderWriter :: IO (CounterEventReader, CounterEventWriter)
+getEventStoreReaderWriter = do
+                            tvar <- eventMapTVar
+                            let reader = tvarEventStoreReader tvar
+                                writer = tvarEventStoreWriter tvar
+                            return (reader, writer)
+
 app :: CounterEventReader -> CounterEventWriter -> Application
 app reader writer = serve counterApiProxy $ server reader writer
 
+runApp :: CounterEventReader -> CounterEventWriter -> IO ()
+runApp reader writer = withStdoutLogger $ \logger -> do
+                                                     let settings = setPort port $ setLogger logger defaultSettings
+                                                     runSettings settings $ app reader writer
+
 main :: IO ()
 main = do
-       tvar <- eventMapTVar
-       let reader = tvarEventStoreReader tvar
-           writer = tvarEventStoreWriter tvar
-       putStr $ "Listening on port " ++ show port ++ "..."
-       run port $ app reader writer
+       (reader, writer) <- getEventStoreReaderWriter
+       putStrLn $ "Listening on port " ++ show port ++ "..."
+       runApp reader writer
